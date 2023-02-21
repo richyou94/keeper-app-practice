1e817ebee06a8f11ba7eb683b74930dc534fa92c
Richard You <richardyou94@gmail.com>
2023-02-20 21:54:08 -0800

    rendering notes from data

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,20 @@ import React from "react";
 import Header from "./Header";
 import Note from "./Note";
 import Footer from "./Footer";
+import notes from "../notes.js";
+console.log(notes);
 
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map((note) => {
+        return <Note 
+        key={note.key}
+        title={note.title}
+        content={note.content}
+        />;
+      })}
       <Footer />
     </div>
   );
--- src/notes.js
+++ src/notes.js
@@ -25,3 +25,5 @@ const notes = [
     }
   ];
   
+
+  export default notes;
