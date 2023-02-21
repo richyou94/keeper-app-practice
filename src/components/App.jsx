e1f9e9347cfa12616b570ef6c2afcea8f77b8b5b
Richard You <richardyou94@gmail.com>
2023-02-21 00:43:50 -0800

    add note button almost working

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
 function App() {
+  const [note, setNote] = useState({
+    title: "",
+    content: "",
+  });
+  const [notes, setNotes] = useState([]);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setNote((prev) => {
+      return {
+        ...prev,
+        [name]: value,
+      };
+    });
+  };
+
+  const addNote = (e) => {
+    e.preventDefault();
+    setNotes((prev) => {
+      return [...prev, note];
+    })
+    setNote((prev) => {
+      return {
+        title: "",
+        content: "",
+      }
+    });
+  };
+
   return (
     <div>
       <Header />
-      <CreateArea />
+      <CreateArea addNote={addNote} handleChange={handleChange} note={note} />
       <Note key={1} title="Note title" cotent="Note content" />
       <Footer />
     </div>
--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-function CreateArea() {
+function CreateArea(props) {
   return (
     <div>
-      <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
+      <form onClick={props.addNote}>
+        <input onChange={props.handleChange} name="title" placeholder="Title" value={props.note.title} />
+        <textarea onChange={props.handleChange} name="content" placeholder="Take a note..." rows="3" value={props.note.content} />
+        <button  >Add</button>
       </form>
     </div>
   );
